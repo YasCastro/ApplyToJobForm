0955b3eb98bac86b114cb567aab0a938af82d86e
Yasmine C. Fortes <yazzcastrof@gmail.com>
2023-12-07 23:18:41 -0300

    first Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TbWorld, TbUser } from 'react-icons/tb';
+import { Box, Text, Flex, Heading, Button, ChakraProvider, Divider, Center, ButtonGroup, Input, RadioGroup, Radio, Stack } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ChakraProvider>
+      <Flex
+        as="nav"
+        backgroundColor="#f5f2f2"
+        color="#696263"
+        direction="column"
+        p={3}>
+        <Flex
+          justify="end"
+          align="center"
+          marginBottom="10px">
+          <TbWorld/>
+          <Text fontSize="sm">Deutsch (Deutschland)</Text>
+          <Center height="15px" width="10px">
+            <Divider orientation='vertical'/>
+          </Center>
+          <TbUser/>
+          <Text fontSize="sm">email@email.com</Text>
+        </Flex>
+        <Flex
+          as="nav"
+          align="center"
+          justify="space-between"
+          padding="15px"
+          p={2}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Heading size="md">Stellenmarkt</Heading>
+          <ButtonGroup>
+            <Button color="#696263" variant="link" paddingRight="15px">Stellenangebote</Button>
+            <Button color="#696263" variant="link" paddingRight="15px">Stellen suchen</Button>
+            <Button color="#696263" variant="link" paddingRight="15px">Kandidat - Home</Button>
+            <Button color="#696263" variant="link" paddingRight="15px">Stellen-Alerts</Button>         
+          </ButtonGroup>
+        </Flex>
+        <Flex maxWidth="80vw" align="center">
+          <Heading>Meine Daten</Heading>
+          <Text>Erforderliches Feld</Text>
+          <Text>Wie haben Sie von uns erfahren?</Text>
+          <Input placeholder=""></Input>  
+          <Text>Haben Sie bereits früher bei ZEISS gearbeitet? Falls ja, beantworten Sie bitte die
+            nachfolgende Frage. Falls nein, machen Sie bitte mit der nächsten Seite weiter.</Text>
+          <RadioGroup>
+            <Stack direction="column">
+              <Radio>Ja</Radio>
+              <Radio>Nein</Radio>
+            </Stack>
+          </RadioGroup>
+
+        </Flex>
+        
+      </Flex>
+      
+    </ChakraProvider>
   );
 }
 
